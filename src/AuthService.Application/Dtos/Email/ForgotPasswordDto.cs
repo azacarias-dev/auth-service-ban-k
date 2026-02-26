e81fadc095abe8e6b67202d87f255a90e3ad6d81
GiancarloCordova <gcordova-2024016@kinal.edu.gt>
2026-02-25 18:09:34 -0600

    Se termino la creacion de los Dtos para manejar la mensajeria por correo

---
+++ src/AuthService.Application/Dtos/Email/ForgotPasswordDto.cs
@@ -0,0 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace AuthService.Application.DTOs.Email;
+
+public class ForgotPasswordDto
+{
+    [Required(ErrorMessage = "El email es obligatorio.")]
+    [EmailAddress(ErrorMessage = "El email no es válido.")]
+    public string Email { get; set; } = string.Empty;
+}
