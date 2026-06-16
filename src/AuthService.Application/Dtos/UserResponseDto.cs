a859c3476087ca526ab595b6530b92b8116ade0f
azacarias-dev <azacarias-2024007@kinal.edu.gt>
2026-06-15 20:02:29 -0600

    refactor: cambiar dto de respuesta al registro

--- src/AuthService.Application/Dtos/UserResponseDto.cs
+++ src/AuthService.Application/Dtos/UserResponseDto.cs
@@ -4,10 +4,13 @@ public class UserResponseDto
 {
     public string Id { get; set; } = string.Empty;
     public string Name { get; set; } = string.Empty;
-    public string Surname { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
     public string Address {get; set;} = string.Empty;
     public string Phone { get; set; } = string.Empty;
+    public string Dpi { get; set; } = string.Empty;
+    public string JobName { get; set; } = string.Empty;
+    public decimal MonthlyIncome { get; set; }
+    public DateTime Birthdate { get; set; }
     public string Role { get; set; } = string.Empty;
     public bool IsActive { get; set; }
     public bool IsEmailVerified { get; set; }
